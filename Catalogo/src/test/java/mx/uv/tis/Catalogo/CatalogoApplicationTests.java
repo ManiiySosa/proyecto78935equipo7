9bce8082f022fb1afcce6505b04cc3788b002acd
Manuel Antonio <zS15013926@estudiantes.uv.mx>
2022-05-18 22:56:48 -0600

    primer commit

---
+++ Catalogo/src/test/java/mx/uv/tis/Catalogo/CatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uv.tis.Catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
