bd8917240096b2f4e619e8d0a76e826784b25dd5
fredy velázquez mtz <zS15013926@estudiantes.uv.mx>
2022-06-01 20:04:59 -0500

    microservicio-catalogo, Dockerfile y despliegue en heroku

--- Catalogo/src/main/java/https/tis_uv_mx/catalogo/package-info.java
+++ Catalogo/src/main/java/https/tis_uv_mx/catalogo/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.06.01 a las 02:30:46 AM CDT 
+// Generado el: 2022.06.01 a las 06:11:09 PM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://tis.uv.mx/catalogo", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
