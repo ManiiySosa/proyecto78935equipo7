83efbb2e0e5610be9d72ac6499057e62124fa5d2
fredy velázquez mtz <zS15013926@estudiantes.uv.mx>
2022-06-03 20:23:41 -0500

    microservicio pedido completado localmente, falta dockerizar y depliegue en heroku

---
+++ Pedidos/src/main/java/https/tis_uv_mx/pedido/AgregarPedidoRequest.java
@@ -0,0 +1,156 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2022.06.03 a las 08:00:34 PM CDT 
+//
+
+
+package https.tis_uv_mx.pedido;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="pedidoId" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="usuarioId" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *         &lt;element name="direccion" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="fecha" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="total" type="{http://www.w3.org/2001/XMLSchema}double"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "pedidoId",
+    "usuarioId",
+    "direccion",
+    "fecha",
+    "total"
+})
+@XmlRootElement(name = "agregarPedidoRequest")
+public class AgregarPedidoRequest {
+
+    protected int pedidoId;
+    protected int usuarioId;
+    @XmlElement(required = true)
+    protected String direccion;
+    @XmlElement(required = true)
+    protected String fecha;
+    protected double total;
+
+    /**
+     * Obtiene el valor de la propiedad pedidoId.
+     * 
+     */
+    public int getPedidoId() {
+        return pedidoId;
+    }
+
+    /**
+     * Define el valor de la propiedad pedidoId.
+     * 
+     */
+    public void setPedidoId(int value) {
+        this.pedidoId = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad usuarioId.
+     * 
+     */
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    /**
+     * Define el valor de la propiedad usuarioId.
+     * 
+     */
+    public void setUsuarioId(int value) {
+        this.usuarioId = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad direccion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * Define el valor de la propiedad direccion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDireccion(String value) {
+        this.direccion = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad fecha.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * Define el valor de la propiedad fecha.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFecha(String value) {
+        this.fecha = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad total.
+     * 
+     */
+    public double getTotal() {
+        return total;
+    }
+
+    /**
+     * Define el valor de la propiedad total.
+     * 
+     */
+    public void setTotal(double value) {
+        this.total = value;
+    }
+
+}
---
+++ Pedidos/src/main/java/mx/uv/tis/pedido/Pedido.java
@@ -0,0 +1,62 @@
+package mx.uv.tis.pedido;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private int pedidoId;
+    private int usuarioId;
+    private String direccion;
+    private String fecha;
+    private double total;
+
+    public Pedido(){
+
+    }
+
+    public int getPedidoId() {
+        return pedidoId;
+    }
+    public void setPedidoId(int pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido [ pedido= " + pedidoId + ", usuario= " + usuarioId
+                + ", direccion= " + direccion + ", fecha=" + fecha +  ", total=" + total + " ] ";
+              
+    }
+    
+    
+}
